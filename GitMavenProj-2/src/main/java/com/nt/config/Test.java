724b769963cfc6935bde0d5a598b2648b6afb8a0
kalyanramnadhagiri <kr2235520@gmail.com>
2025-11-13 11:47:08 +0530

    start() added to Test class

--- GitMavenProj-2/src/main/java/com/nt/config/Test.java
+++ GitMavenProj-2/src/main/java/com/nt/config/Test.java
@@ -4,6 +4,11 @@ public class Test
 {
 	public static void main(String[] args) {
 		System.out.println("Hii ......");
+		start();
 	}
-
+	public static void start()
+	{
+		System.out.println("Jorney Started From Hyd");
+		System.out.println("Journey Ended at Kashmir");
+	}
 }
